9f9983b06b3ccb199b7fbcdd12592f3e25d3b341
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-30 14:51:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence)
+return_sentence = []
 orig_sentence =  sentence.split 
 orig_sentence.each do |word|
+  return_sentence << word.reverse
+end
+return_sentence.join(" ")
 end
